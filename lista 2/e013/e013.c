649ef85af65bd1fa596ad3c6648d5b56f4910970
Matheus Ferreira <161968118+matheusferreir4@users.noreply.github.com>
2025-01-01 16:16:17 -0300

    Update e013.c

--- lista 2/e013/e013.c
+++ lista 2/e013/e013.c
@@ -29,3 +29,38 @@ int main (){
 
     return 0;
 }
+
+/* 01/01/2025
+
+    #include <stdio.h>
+
+int main(){
+    int sexo;
+    float altura;
+    while(1){
+        printf("\nSexo (1- FEM) ou (2- MAS):");
+        scanf("%d", &sexo);
+        
+        if(sexo != 1 && sexo != 2){
+            break;
+        }
+        
+        switch(sexo){
+            case 1:
+                printf("\nInsira sua altura:");
+                scanf("%f", &altura);
+                
+                printf("\nSeu peso ideal é: %.2f\n", (altura != 0) ? ((62.1 * altura) - 44.7) : 0);
+            break;
+            
+            case 2:
+                printf("\nInsira sua altura:");
+                scanf("%f", &altura);
+                
+                printf("\nSeu peso ideal é: %.2f\n", (altura != 0) ? ((72.7 * altura) - 58.0) : 0);
+            break;
+        }
+    }
+    
+    return 0;
+} */
